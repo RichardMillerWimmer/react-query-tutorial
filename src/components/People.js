af278f5de9b84bb08217b930f687c173fcc4c48c
Richard Miller <richardmmill@gmail.com>
2021-08-31 16:37:26 -0400

    person.js

--- src/components/People.js
+++ src/components/People.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from 'react-query';
+import { Person } from './Person';
+
+const fetchPeople = async () => {
+    const res = await fetch('http://swapi.dev/api/people/');
+    return res.json();
+}
 
 export const People = () => {
+    const { data, status } = useQuery('people', fetchPeople);
+    console.log(data)
+
     return (
         <div>
             <h2>People</h2>
+            
+            { status === 'loading' && (
+                <div>Loading people data...</div>
+            )}
+             { status === 'success' && (
+                <div>
+                    {data.results.map(person => <Person key={person.name} person={person} />)}
+                </div>
+            )}
+            { status === 'error' && (
+                <div>Error fetching people data</div>
+            )}
         </div>
     )
 }
-
